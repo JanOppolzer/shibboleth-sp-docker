5012587ad2ed7fce0c9afb2df1c776c927946b4d
Jan Oppolzer <jan@oppolzer.cz>
2017-11-11 09:56:53 +0100

    Update run.sh
    It's possible to specify HTTP and HTTPS ports on which to publish ports
    exposed from the container.

--- run.sh
+++ run.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
+PORT_HTTP=80
+PORT_HTTPS=443
+
+if [[ -z $1 ]]; then
+    echo "No port for HTTP specified, using 80."
+else
+    PORT_HTTP=$1
+fi
+
+if [[ -z $2 ]]; then
+    echo "No port for HTTPS specified, using 443."
+else
+    PORT_HTTPS=$2
+fi
+
 docker container run \
     -it \
     --rm \
     --detach \
     --name shibboleth-sp \
     --hostname shibboleth-sp \
-    -p 8080:80 -p 8443:443 \
+    -p $PORT_HTTP:80 -p $PORT_HTTPS:443 \
     shibboleth-sp
 
